9f00adc98d8508536c87d47222ed996747b5f205
meditat0r <2523398873@qq.com>
2021-11-25 20:36:25 +0800

    eureka注册中心

---
+++ springcloud-eureka-7001/src/main/java/com/kuang/springcloud/EurekaServer_7001.java
@@ -0,0 +1,17 @@
+package com.kuang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author meditat0r
+ * @create 2021/11/25 18:21
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer_7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_7001.class, args);
+    }
+}
