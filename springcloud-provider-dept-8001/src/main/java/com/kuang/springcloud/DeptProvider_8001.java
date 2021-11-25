25b72217e8eb585375843e77fcc1541c0864681c
meditat0r <2523398873@qq.com>
2021-11-25 16:18:05 +0800

    provider服务

---
+++ springcloud-provider-dept-8001/src/main/java/com/kuang/springcloud/DeptProvider_8001.java
@@ -0,0 +1,15 @@
+package com.kuang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author meditat0r
+ * @create 2021/11/25 15:40
+ */
+@SpringBootApplication
+public class DeptProvider_8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider_8001.class, args);
+    }
+}
---
+++ springcloud-provider-dept-8001/src/main/java/com/kuang/springcloud/controller/DeptController.java
@@ -0,0 +1,36 @@
+package com.kuang.springcloud.controller;
+
+import com.kuang.springcloud.pojo.Dept;
+import com.kuang.springcloud.service.DeptService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @Author meditat0r
+ * @create 2021/11/25 15:32
+ */
+@RestController
+public class DeptController {
+    @Resource
+    private DeptService deptService;
+
+    @PostMapping("/dept/add")
+    public boolean addDept(Dept dept){
+        return deptService.addDept(dept);
+    }
+
+    @GetMapping("/dept/get/{id}")
+    public Dept get(@PathVariable Long id){
+        return deptService.queryById(id);
+    }
+
+    @GetMapping("/dept/list")
+    public List<Dept> queryAll(){
+        return deptService.queryAll();
+    }
+}
