5dc2bdb77f2d539fcaeb2cece0e25acf59f3c519
meditat0r <2523398873@qq.com>
2021-11-26 17:20:52 +0800

    Hystrix Dashboard服务监控

---
+++ springcloud-consumer-hystrix-dashbroad/src/main/java/com/kuang/springcloud/DeptConsumerDashboard_9001.java
@@ -0,0 +1,17 @@
+package com.kuang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Author meditat0r
+ * @create 2021/11/26 16:36
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DeptConsumerDashboard_9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumerDashboard_9001.class, args);
+    }
+}
